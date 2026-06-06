15a5912f4f2642c16715ea32376fee5f1992d56d
ElenaVannini <116877226+ElenaVannini@users.noreply.github.com>
2026-06-06 21:59:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -19,25 +19,44 @@ const MyComponents = {
 };
 
 (async () => {
-    const title = await MyComponents.Title()
-    title.querySelector('h2').innerText = 'Title'
-    const titleb = await MyComponents.Title()
-    titleb.querySelector('h2').innerText = 'Title1'
+
+    const TitleData = [
+        {
+            href: 'https://it.wikipedia.org/',
+            about: 'About Me',
+            svg: '',
+            button: '',
+        }
+    ]
+
+    for (const data of TitleData) {
+        const title = await MyComponents.Title()
+        title.querySelector('a').href = data.href
+        title.querySelector('a').innerText = data.about
+        title.querySelector('svg').innerHTML = data.svg
+        title.querySelector('button').button = data.button
+    }
 
 
     const CardData = [
         {
-            src: 'https://picsum.photos/200/300',
+            href: 'https://www.google.com/',
+            src: 'https://picsum.photos/200/300?random=1',
             title: 'Card Title 1'
         },
         {
-            src: 'https://picsum.photos/200/300',
+            src: 'https://picsum.photos/200/300?random=2',
             title: 'Card Title 2'
+        },
+        {
+            src: 'https://picsum.photos/200/300?random=3',
+            title: 'Card Title 3'
         }
     ]
 
     for (const data of CardData) {
         const card = await MyComponents.Card()
+        card.querySelector('a').href = data.href
         card.querySelector('h2').innerText = data.title
         card.querySelector('img').src = data.src
     }
